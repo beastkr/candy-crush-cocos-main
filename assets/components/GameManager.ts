5fef6e63bef3055ea360345c172abcaafaebbd53
beastkr <dangcvptag3@gmail.com>
2025-08-19 09:06:25 +0700

    a

--- assets/components/GameManager.ts
+++ assets/components/GameManager.ts
@@ -7,78 +7,19 @@ const { ccclass, property } = _decorator
 
 @ccclass('GameManager')
 export default class GameManager extends Component {
-    @property(TileGrid)
-    public grid: TileGrid = new TileGrid()
+    public static instance: GameManager | null = null
     @property(TilePool)
-    public pool: TilePool = new TilePool()
-    firstChosen: Tile | undefined = undefined
-    secondChosen: Tile | undefined = undefined
-    @property(Prefab)
-    public tilePrefab: Prefab | null = null
-    protected __preload(): void {
-        if (!this.grid) {
-            console.error('TileGrid is not assigned in GameManager')
-        }
-        if (!this.pool) {
-            console.error('TilePool is not assigned in GameManager')
-        }
-        if (!this.tilePrefab) {
-            console.error('TilePrefab is not assigned in GameManager')
-        }
-    }
-    protected async start(): Promise<void> {
-        this.pool?.setupPrefab(this.tilePrefab as Prefab)
-        await this.createTable()
-    }
-    async createTable(): Promise<void> {
-        this.pool?.initPool(GameConfig.GridHeight * GameConfig.GridWidth)
-        this.pool?.getList().forEach((element) => {
-            this.node.addChild(element.node)
-        })
-        this.grid?.initTable()
-        await this.grid?.fillTable(this.pool as TilePool)
-        this.grid?.setUpManager(this)
-    }
-    unChose(): void {
-        this.grid.choosingTile = null
-        if (this.firstChosen) {
-            this.firstChosen.release()
-        }
-        if (this.secondChosen) {
-            this.secondChosen.release()
-        }
-    }
+    tilePool: TilePool | null = null
 
-    tileUp() {
-        this.firstChosen = undefined
-        this.secondChosen = undefined
-    }
-    async Chose(tile: Tile): Promise<void> {
-        this.grid.choosingTile = tile
-        if (!this.grid.canChoose) return
-        if (tile.coords === this.firstChosen?.coords) {
-            console.log('Tile already chosen, unchoosing it')
-            this.unChose()
+    public start(): void {
+        if (GameManager) {
+            this.destroy()
             return
         }
-        if (!this.firstChosen) {
-            this.firstChosen = tile
-            tile.chose()
-        } else {
-            this.secondChosen = tile
-            tile.chose()
-            this.unChose() // Unchoose immediately after selecting second tile
-            await this.Move()
-        }
+        GameManager.instance = this
+        this.setUp()
     }
-    async Move(): Promise<void> {
-        if (
-            this.firstChosen &&
-            this.secondChosen &&
-            this.grid.canSwap(this.firstChosen, this.secondChosen)
-        ) {
-            await this.grid.swap(this.firstChosen, this.secondChosen)
-        }
-        this.tileUp()
-    }
+
+    setUp() { }
+
 }
